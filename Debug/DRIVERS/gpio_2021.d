4c4026c02094b8c48eb7058b099ccbf3dc513229
Ariel Arciniega <soyscooby@gmail.com>
2022-11-17 14:41:26 -0600

    Primer repo

---
+++ Debug/DRIVERS/gpio_2021.d
@@ -0,0 +1,66 @@
+# FIXED
+
+DRIVERS/gpio_2021.obj: ../DRIVERS/gpio_2021.c
+DRIVERS/gpio_2021.obj: ../DRIVERS/gpio_2021.h
+DRIVERS/gpio_2021.obj: C:/Users/User/workspace_v12/Workshop2_V3/DRIVERS/gpio_config_2021.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp_compatibility.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r_classic.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+DRIVERS/gpio_2021.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/system_msp432p401r.h
+
+../DRIVERS/gpio_2021.c:
+
+../DRIVERS/gpio_2021.h:
+
+C:/Users/User/workspace_v12/Workshop2_V3/DRIVERS/gpio_config_2021.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp_compatibility.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r_classic.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/system_msp432p401r.h:
+
